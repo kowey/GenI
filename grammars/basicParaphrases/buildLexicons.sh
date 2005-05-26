582155c6135c01d97c21f9c5127895e3a617f0c3
kow <kow@loria.fr>
2005-05-26 15:27:34 +0000

    more flexible buildLexicons.sh

--- grammars/basicParaphrases/buildLexicons.sh
+++ grammars/basicParaphrases/buildLexicons.sh
@@ -2,13 +2,14 @@
 
 GDEPATH=${HOME}/phd/tools/GDE
 
-TMPLEX=lex/tmp
-LEX_COMM=lex/commonLexicon.lex
+LEXDIR=lex
+TMPLEX=${LEXDIR}/tmp
+LEX_COMM=${LEXDIR}/commonLexicon.lex
 
 touch $TMPLEX
-for i in lex/*.lex; do
+for i in ${LEXDIR}/*.lex; do
   LEX=`basename ${i} .lex`
-  LEX_IN=lex/${LEX}.lex
+  LEX_IN=${LEXDIR}/${LEX}.lex
   LEX_OUT=prefil/${LEX}.prefil
   if [ ! -e $LEX_OUT -o\
        -n "`find ${LEX_IN} -newer ${LEX_OUT} -print`" -o\
