8a2f270d9563b1711d852f342eb8a32e139dd994
kow <kow@loria.fr>
2005-06-17 12:48:25 +0000

    (perf) testing apparatus

--- etc/runprofiler.sh
+++ etc/runprofiler.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
 if [ $# -lt 1 ]; then
-  echo "usage: $0 profoption"
+  echo "usage: $0 [switches]"
   exit 1
 fi
 
-SUFFIX=${1}
-STEM=profres-${SUFFIX}
+OPTIONS=$@
+PROG=debugger-geni
+SUFFIX=`date +%d-%H%M-%S`
+STEM=heap-${SUFFIX}
 
 cd `dirname $0`/..
 
-rm debugger-geni-${SUFFIX}.hp
-./debugger-geni +RTS -K50m -${SUFFIX}
-mv debugger-geni.hp ${STEM}.hp
+./${PROG} +RTS -K50m ${OPTIONS}
+mv ${PROG}.hp ${STEM}.hp
 sed -e "/alex/d" -e "/parser/d" ${STEM}.hp > ${STEM}-trim.hp
 hp2ps ${STEM}-trim.hp
+if [ $? -eq 0 ]; then
+  rm heap-latest.ps
+  ln -s ${STEM}-trim.ps heap-latest.ps
+  gv -seascape heap-latest.ps &
+fi
