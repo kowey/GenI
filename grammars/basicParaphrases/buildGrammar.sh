7c1a454b3852ec420fe09a5cf84fecf10092e3b7
kow <kow@loria.fr>
2005-09-19 15:29:14 +0000

    replaced grammar/lexicon scripts by Makefile much cleaner and more agreeable to use

--- grammars/basicParaphrases/buildGrammar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-GRAMMAR=headSem
-GRAMMAR_TMP=${GRAMMAR}.tmp
-
-cd gram
-MetaTAG ${GRAMMAR}.mg       --chk -c ${GRAMMAR_TMP}.rec 
-CheckTAG ${GRAMMAR_TMP}.rec --chk -c ${GRAMMAR}.rec 
-rm ${GRAMMAR_TMP}.rec
