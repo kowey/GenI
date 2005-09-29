a4e33ba45b6add37ba71ba9dc2db55075703b8de
kow <kow@loria.fr>
2005-09-29 20:01:34 +0000

    (gram rm) removed all non-example grammars

--- grammars/enlg2005/buildlite.sh
+++
@@ -1,5 +0,0 @@
-cd `dirname $0`
-cd gram
-MetaTAG --xml -o semFrenchLite.xml semFrenchLite.mg 
-rm semFrenchLite.geni
-../../../geniconvert --macros < semFrenchLite.xml > semFrenchLite.geni
--- grammars/esslli05/buildlite.sh
+++
@@ -1,3 +0,0 @@
-MetaTAG --xml -o semFrenchLite.xml semFrenchLite.mg
-rm semFrenchLite.geni
-../../geniconvert --macros < semFrenchLite.xml > semFrenchLite.geni
