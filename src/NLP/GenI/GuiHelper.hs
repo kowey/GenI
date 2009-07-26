e9c462ae02551bb6b97dc10a60a687d43a2186e3
Eric Kow <E.Y.Kow@brighton.ac.uk>
2009-07-26 09:12:39 +0000

    Switch GUI to use Control.Monad.State.Strict as well.

--- src/NLP/GenI/GuiHelper.hs
+++ src/NLP/GenI/GuiHelper.hs
@@ -22,7 +22,7 @@ import Graphics.UI.WX
 -- import Graphics.UI.WXCore
 
 import qualified Control.Monad as Monad 
-import Control.Monad.State ( execStateT, runState )
+import Control.Monad.State.Strict ( execStateT, runState )
 import qualified Data.Map as Map
 
 import Data.IORef
