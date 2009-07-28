67a851d460c83b7db89b76b7b56c86022ced177a
Eric Kow <E.Y.Kow@brighton.ac.uk>
2009-07-28 09:30:53 +0000

    Fix unification bug for [GeniVal]. I wasn't doing back-propagation. This bug is still there in a deeper sense, but I think I'm doing back-propagation in other places where I'm doing unification by hand.

--- src/NLP/GenI/GeniVal.hs
+++ src/NLP/GenI/GeniVal.hs
@@ -88,18 +88,21 @@ type Subst = Map.Map String GeniVal
 --   the result of unification and \verb!s! is a list of substitutions that
 --   this unification results in.
 unify :: Monad m => [GeniVal] -> [GeniVal] -> m ([GeniVal], Subst)
-unify [] l2 = return (l2, Map.empty)
-unify l1 [] = return (l1, Map.empty)
-unify (h1:t1) (h2:t2) =
-  case unifyOne h1 h2 of
-  Failure -> fail $ "unification failure between " ++ show h1 ++ " and " ++ show h2
-  SuccessRep v g -> prepend `liftM` unify t1b t2b
-                    where
-                     s   = (v,g)
-                     t1b = replaceOne s t1
-                     t2b = replaceOne s t2
-                     prepend = (g:) *** prependToSubst s
-  SuccessSans g  -> first (g:) `liftM` unify t1 t2
+unify l1 l2 = repropagate `liftM` helper l1 l2
+ where
+  repropagate (xs, sub) = (replace sub xs, sub)
+  helper [] l2 = return (l2, Map.empty)
+  helper l1 [] = return (l1, Map.empty)
+  helper (h1:t1) (h2:t2) =
+    case unifyOne h1 h2 of
+    Failure -> fail $ "unification failure between " ++ show h1 ++ " and " ++ show h2
+    SuccessRep v g -> prepend `liftM` unify t1b t2b
+                      where
+                       s   = (v,g)
+                       t1b = replaceOne s t1
+                       t2b = replaceOne s t2
+                       prepend = (g:) *** prependToSubst s
+    SuccessSans g  -> first (g:) `liftM` unify t1 t2
 
 -- | Note that the first Subst is assumed to come chronologically
 --   before the second one; so merging @{ X -> Y }@ and @{ Y -> 3 }@
