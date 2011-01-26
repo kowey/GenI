aa8912cec0bebe021dca70c1d912308ecc83dbcc
Eric Kow <E.Y.Kow@brighton.ac.uk>
2011-01-26 13:48:06 +0000

    Generic boolean expressions?

---
+++ src/BoolExp.hs
@@ -0,0 +1,10 @@
+data BoolExp a = Cond a
+               | And (BoolExp a) (BoolExp a)
+               | Or  (BoolExp a) (BoolExp a)
+               | Not (BoolExp a)
+
+check :: (a -> Bool) -> BoolExp a -> Bool
+check f (Cond x)  = f x
+check f (And x y) = check (f x) && check (f y)
+check f (Or x y)  = check (f x) || check (f y)
+check f (Not x)   = not (check f x)
