c766df703848c18dfaa5f98ef02b772e41486006
Eric Kow <eric.kow@gmail.com>
2012-02-07 11:36:58 +0000

    Forgetten files from the big package rearrangement.

---
+++ geni-gui/Setup.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE CPP #-}
+
+import Control.Monad (foldM_, forM_)
+import Data.Maybe ( fromMaybe )
+import System.Cmd
+import System.Exit
+import System.Info (os)
+import System.FilePath
+import System.Directory ( doesFileExist, copyFile, removeFile, createDirectoryIfMissing )
+
+import Distribution.PackageDescription
+import Distribution.MacOSX
+import Distribution.Simple.Setup
+import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo
+
+main :: IO ()
+main = defaultMainWithHooks $ simpleUserHooks
+    { postInst = appBundleInstallHook [geniApp DoNotChase]
+    , postBuild = appBundleBuildHook  [geniApp DoNotChase]
+    }
+
+geniApp :: ChaseDeps -> MacApp
+geniApp =
+  MacApp "geni-gui"
+         (Just "macstuff/wxmac.icns")
+         (Just "macstuff/Info.plist")
+         []
+         []
---
+++ geni/geni.hs
@@ -0,0 +1,70 @@
+-- GenI surface realiser
+-- Copyright (C) 2005 Carlos Areces and Eric Kow
+--
+-- This program is free software; you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation; either version 2
+-- of the License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+{-# LANGUAGE CPP #-}
+module Main where
+
+import Control.Applicative ((<$>))
+import Data.IORef(newIORef)
+import Data.Typeable( Typeable )
+import Data.Version ( showVersion )
+import System.Environment(getArgs, getProgName)
+
+import Paths_GenI ( version )
+
+import NLP.GenI.Geni(emptyProgState)
+import NLP.GenI.Console(consoleGeni)
+import NLP.GenI.Configuration (treatArgs, optionsForStandardGenI, processInstructions,
+                               usage, optionsSections, Params,
+                               hasFlagP, BatchDirFlg(..), DisableGuiFlg(..),
+                               DumpDerivationFlg(..),  FromStdinFlg(..),
+                               HelpFlg(..), VersionFlg(..), TestCaseFlg(..),
+                               readGlobalConfig, setLoggers
+                              )
+import NLP.GenI.Configuration(setFlagP)
+import NLP.GenI.Geni( ProgState(..) )
+
+main :: IO ()
+main = do
+  args  <- getArgs
+  confArgs <- forceGuiFlag <$> (processInstructions =<< treatArgs optionsForStandardGenI args)
+  mainWithState (emptyProgState confArgs)
+
+mainWithState :: ProgState -> IO ()
+mainWithState pst = do
+  pname <- getProgName
+  maybe (return ()) setLoggers =<< readGlobalConfig
+  pstRef <- newIORef pst
+  let has :: (Typeable f, Typeable x) => (x -> f) -> Bool
+      has = flip hasFlagP (pa pst)
+      mustRunInConsole = has DumpDerivationFlg || has FromStdinFlg || has BatchDirFlg
+      canRunInConsole  = has TestCaseFlg
+  case () of
+   _ | has HelpFlg               -> putStrLn (usage optionsSections pname)
+     | has VersionFlg            -> putStrLn (pname ++ " " ++ showVersion version)
+     | mustRunInConsole          -> consoleGeni pstRef
+     | canRunInConsole           -> consoleGeni pstRef
+     | otherwise                 -> fail $ unlines
+        [ "genibatch must either be run..."
+        , " - with a test case specified"
+        , " - with a batch directory specified or"
+        , " - with --dump"
+        , " - with --from-stdin"
+        ]
+
+forceGuiFlag :: Params -> Params
+forceGuiFlag = setFlagP DisableGuiFlg ()
