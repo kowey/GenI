125b199e35cf4e35551d43ef1e9147ef5c1cbe5e
Eric Kow <eric.kow@gmail.com>
2012-12-06 17:09:11 +0000

    Reindent NLP.GenI.LexicalSelection with some minor refactors. I'd like to shift everything to the 4 space indent standard

--- src/NLP/GenI/LexicalSelection.hs
+++ src/NLP/GenI/LexicalSelection.hs
@@ -17,8 +17,10 @@
 
 -- | This module performs the core of lexical selection and anchoring.
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
+
 module NLP.GenI.LexicalSelection
-where
+  where
 
 import           Control.Applicative             ((<$>))
 import           Control.Arrow                   ((***))
@@ -79,14 +81,14 @@ type LexicalSelector sem = Macros -> Lexicon -> sem -> IO LexicalSelection
 
 -- | The result of the lexical selection process
 data LexicalSelection = LexicalSelection
-      { -- | the main result: a set of elementary trees (ie. anchored trees)
-        lsAnchored   :: [TagElem]
-        -- | if available, lexical entries that were used to produce anchored
-        --   trees (useful for identifying anchoring failure)
-      , lsLexEntries :: [LexEntry]
-        -- | HINT: use 'Data.Monoid.mempty' to initialise to empty
-      , lsWarnings   :: GeniWarnings
-      }
+    { -- | the main result: a set of elementary trees (ie. anchored trees)
+      lsAnchored   :: [TagElem]
+      -- | if available, lexical entries that were used to produce anchored
+      --   trees (useful for identifying anchoring failure)
+    , lsLexEntries :: [LexEntry]
+      -- | HINT: use 'Data.Monoid.mempty' to initialise to empty
+    , lsWarnings   :: GeniWarnings
+    }
 
 -- | Performs standard GenI lexical selection as described in
 --   <http://projects.haskell.org/GenI/manual/lexical-selection.html>
@@ -121,9 +123,10 @@ defaultLexicalSelection grammar lexicon sem =
 --   because it's possible for the trees to add semantics through
 --   unification.
 missingLexEntries :: [TagElem] -> [LexEntry] -> [LexEntry]
-missingLexEntries cands = filter treeless
- where
-  treeless l = isNothing $ find (\t -> tsemantics t == isemantics l) cands
+missingLexEntries cands =
+    filter treeless
+  where
+    treeless l = isNothing $ find (\t -> tsemantics t == isemantics l) cands
 
 -- ----------------------------------------------------------------------
 -- * Selecting candidate lemmas
@@ -139,16 +142,19 @@ defaultLexicalChoice slex (tsem,_,_) = chooseCandI tsem slex
 --   @l@ as a lexical selection candidate.
 chooseCandI :: Sem -> [LexEntry] -> [LexEntry]
 chooseCandI tsem cand =
-  let replaceLex i (sem,sub) =
+    nub $ concatMap helper cand
+  where
+    replaceLex i (sem,sub) =
         (replace sub i) { isemantics = sem }
-      --
-      helper :: LexEntry -> [LexEntry]
-      helper l = if null sem then [l]
-                 else map (replaceLex l) psubsem
-        where psubsem = sem `subsumeSem` tsem
-              sem = isemantics l
-      --
-  in nub $ concatMap helper cand
+    --
+    helper :: LexEntry -> [LexEntry]
+    helper l =
+        if null sem
+            then [l]
+            else map (replaceLex l) psubsem
+      where
+        psubsem = sem `subsumeSem` tsem
+        sem = isemantics l
 
 -- | `mergeSynonyms' is a factorisation technique that uses
 --   atomic disjunction to merge all synonyms into a single lexical
@@ -161,11 +167,14 @@ chooseCandI tsem cand =
 --  just to add a check that the path equations match exactly.
 mergeSynonyms :: [LexEntry] -> [LexEntry]
 mergeSynonyms lexEntry =
-  let mergeFn l1 l2 = l1 { iword = (FL.++) (iword l1) (iword l2) }
-      keyFn l = (ifamname l, isemantics l)
-      synMap = foldr helper Map.empty lexEntry
-        where helper x acc = Map.insertWith mergeFn (keyFn x) x acc
-  in Map.elems synMap
+    Map.elems synMap
+  where
+    mergeFn l1 l2 = l1 { iword = (FL.++) (iword l1) (iword l2) }
+    keyFn l = (ifamname l, isemantics l)
+    synMap  =
+        foldr helper Map.empty lexEntry
+      where
+        helper x acc = Map.insertWith mergeFn (keyFn x) x acc
 
 -- --------------------------------------------------------------------
 -- * Anchoring
@@ -187,25 +196,25 @@ lexTell x = lift (tell [x])
 --   This function may be useful if you are implementing your own lexical selection
 --   functions, and you want GenI to take over after you've given it a @[LexEntry]@
 defaultAnchoring :: SemInput -> Macros -> [LexEntry] -> LexicalSelection
-defaultAnchoring (tsem,_,_) grammar lexCands =
-  LexicalSelection { lsAnchored   = cands
-                   , lsLexEntries = lexCands
-                   , lsWarnings   = mconcat [ lexWarnings, coanchorWarnings, errs ]
-                   }
- where
-  combinations  = map (combineList tsem grammar) lexCands
-  cands         = concatMap snd combinations
-  errs          = mkGeniWarnings . concat $ zipWith mkWarnings lexCands (map fst combinations)
-  mkWarnings l  = map (LexWarning [l] . LexCombineOneSchemaFailed)
-  coanchorWarnings = mkGeniWarnings $ do -- list monad
-    l     <- lexCands
-    let xs = filter (\p -> pfamily p == ifamname l) grammar
-    (c,n) <- Map.toList . histogram $ concatMap (missingCoanchors l) xs
-    return (LexWarning [l] (MissingCoanchors c n))
-  lexWarnings = mkGeniWarnings $ case missingLexEntries cands lexCands of
-                  [] -> []
-                  xs -> [LexWarning xs LexCombineAllSchemataFailed]
-
+defaultAnchoring (tsem,_,_) grammar lexCands = LexicalSelection
+    { lsAnchored   = cands
+    , lsLexEntries = lexCands
+    , lsWarnings   = mconcat [ lexWarnings, coanchorWarnings, errs ]
+    }
+  where
+    combinations  = map (combineList tsem grammar) lexCands
+    cands         = concatMap snd combinations
+    errs          = mkGeniWarnings . concat $ zipWith mkWarnings lexCands (map fst combinations)
+    mkWarnings l  = map (LexWarning [l] . LexCombineOneSchemaFailed)
+    coanchorWarnings = mkGeniWarnings $ do -- list monad
+        l     <- lexCands
+        let xs = filter (\p -> pfamily p == ifamname l) grammar
+        (c,n) <- Map.toList . histogram $ concatMap (missingCoanchors l) xs
+        return (LexWarning [l] (MissingCoanchors c n))
+    lexWarnings = mkGeniWarnings $
+        case missingLexEntries cands lexCands of
+            [] -> []
+            xs -> [LexWarning xs LexCombineAllSchemataFailed]
 
 -- ----------------------------------------------------------------------
 -- ** Combination
@@ -216,20 +225,20 @@ defaultAnchoring (tsem,_,_) grammar lexCands =
 combineList :: Sem -> Macros -> LexEntry
             -> ([LexCombineError],[TagElem]) -- ^ any warnings, plus the results
 combineList tsem gram lexitem =
-  case [ t | t <- gram, pfamily t == tn ] of
-       []   -> ([FamilyNotFoundError tn],[])
-       macs -> squish . swap . unzip $ map (\m -> runWriter . runMaybeT $ combineOne tsem lexitem m) macs
+    case [ t | t <- gram, pfamily t == tn ] of
+        []   -> ([FamilyNotFoundError tn],[])
+        macs -> squish . swap . unzip $ map (\m -> runWriter . runMaybeT $ combineOne tsem lexitem m) macs
   where
-   tn = ifamname lexitem
-   swap (x,y) = (y,x)
-   squish = (compressLexCombineErrors . concat) *** (concat . catMaybes)
+    tn = ifamname lexitem
+    swap (x,y) = (y,x)
+    squish = (compressLexCombineErrors . concat) *** (concat . catMaybes)
 
 -- | Combine a single tree with its lexical item to form a bonafide TagElem.
 --   This process can fail, however, because of filtering or enrichement
 combineOne :: Sem -> LexEntry -> SchemaTree -> LexCombine [TagElem]
-combineOne tsem lexRaw eRaw = -- Maybe monad
- -- trace ("\n" ++ (show wt)) $
- do let l1 = finaliseVars "-l" lexRaw
+combineOne tsem lexRaw eRaw = do -- Maybe monad
+    -- trace ("\n" ++ (show wt)) $
+    let l1 = finaliseVars "-l" lexRaw
         e1 = finaliseVars "-t" eRaw
     (l,e) <- unifyParamsWithWarning (l1,e1)
              >>= unifyInterfaceUsing iinterface
@@ -255,39 +264,39 @@ combineOne tsem lexRaw eRaw = -- Maybe monad
               , ttrace      = ptrace e
               }
     semUnifications <- case unifySem (isemantics l) (fromMaybe [] $ psemantics e) of
-                         [] -> do lexTell (SchemaError [pidname e] (StringError "could not unify lemma and schema semantics"))
-                                  fail ""
-                         xs -> return xs
+                           [] -> croak e "could not unify lemma and schema semantics"
+                           xs -> return xs
     return $ concatMap (anonymiseSingletons . finaliseSemantics template) semUnifications
- where
-  croak :: SchemaTree -> Text -> LexCombine a
-  croak t msg = do
-      lexTell (SchemaError [pidname t] (StringError msg))
-      mzero
-  finaliseSemantics template (sem,sub) =
-    do (sem2,sub2) <- sem `subsumeSem` replace sub tsem
-       return $ replace sub2 $ template { tsemantics = sem2 }
-  unifyParamsWithWarning (l,t) =
-   -- trace ("unify params " ++ wt) $
-   let lp = iparams l
+  where
+    croak :: SchemaTree -> Text -> LexCombine a
+    croak t msg = do
+        lexTell (SchemaError [pidname t] (StringError msg))
+        mzero
+    finaliseSemantics template (sem,sub) = do
+        (sem2,sub2) <- sem `subsumeSem` replace sub tsem
+        return $ replace sub2 $ template { tsemantics = sem2 }
+    unifyParamsWithWarning (l,t) =
+        if length lp /= length tp
+            then croak t "Parameter length mismatch"
+            else case unify lp tp of
+                     Left msg           ->
+                         croak t ("Parameter unification error: " <> msg)
+                     Right (ps2, subst) ->
+                         let t2 = (replace subst t) { params = ps2 }
+                         in return (replace subst l, t2)
+     where
+       lp = iparams l
        tp = params t
-   in if length lp /= length tp
-      then croak t "Parameter length mismatch"
-      else case unify lp tp of
-             Left msg           -> croak t ("Parameter unification error: " <> msg)
-             Right (ps2, subst) -> return (replace subst l, t2)
-                                  where t2 = (replace subst t) { params = ps2 }
-  unifyInterfaceUsing ifn (l,e) =
-    -- trace ("unify interface" ++ wt) $
-    case unifyFeat (ifn l) (pinterface e) of
-    Left msg             -> croak e ("Interface unification error: " <> msg)
-    Right (int2, fsubst) -> return (replace fsubst l, e2)
-                           where e2 = (replace fsubst e) { pinterface = int2 }
-  --
-  enrichWithWarning (l,e) =
-    -- trace ("enrich" ++ wt) $
-    do e2 <- enrich l e
-       return (l,e2)
+    unifyInterfaceUsing ifn (l,e) =
+        -- trace ("unify interface" ++ wt) $
+        case unifyFeat (ifn l) (pinterface e) of
+            Left msg             ->
+                 croak e ("Interface unification error: " <> msg)
+            Right (int2, fsubst) ->
+                 let e2 = (replace fsubst e) { pinterface = int2 }
+                 in  return (replace fsubst l, e2)
+    --
+    enrichWithWarning (l,e) = (l,) <$>  enrich l e
 
 -- ----------------------------------------------------------------------
 -- ** Enrichment
@@ -296,20 +305,20 @@ combineOne tsem lexRaw eRaw = -- Maybe monad
 -- | See <http://projects.haskell.org/manual/lexical-selection>
 --   on enrichement
 enrich :: LexEntry -> SchemaTree -> LexCombine SchemaTree
-enrich l t =
- do -- separate into interface/anchor/named
+enrich l t = do
+    -- separate into interface/anchor/named
     (intE, namedE) <- lift $ lexEquations l
     -- enrich the interface and everything else
     t2 <- foldM enrichInterface t intE
     -- enrich everything else
     foldM enrichBy t2 namedE
- where
-  enrichInterface tx en =
-    case hush (unifyFeat [en] (pinterface tx)) of
-      Nothing -> lexTell (ifaceEnrichErr en) >> fail ""
-      Just (i2, isubs) -> return $ (replace isubs tx) { pinterface = i2 }
-  ifaceEnrichErr (AvPair loc _) =
-       SchemaError [pidname t] (EnrichError (PeqInterface loc))
+  where
+    enrichInterface tx en =
+        case hush (unifyFeat [en] (pinterface tx)) of
+            Nothing -> lexTell (ifaceEnrichErr en) >> fail ""
+            Just (i2, isubs) -> return $ (replace isubs tx) { pinterface = i2 }
+    ifaceEnrichErr (AvPair loc _) =
+        SchemaError [pidname t] (EnrichError (PeqInterface loc))
 
 -- *** 'enrich' helpers
 
@@ -318,33 +327,34 @@ enrichBy :: SchemaTree
          -> PathEqPair
          -> LexCombine SchemaTree
 enrichBy t eq@(eqLhs, _) =
-  case maybeEnrichBy t eq of
-    Nothing -> lexTell enrichErr >> return t
-    Just (t2,_) -> return t2
- where
-  enrichErr = SchemaError [pidname t] (EnrichError (PeqJust eqLhs))
+    case maybeEnrichBy t eq of
+        Nothing     -> lexTell enrichErr >> return t
+        Just (t2,_) -> return t2
+  where
+    enrichErr = SchemaError [pidname t] (EnrichError (PeqJust eqLhs))
 
 -- | Helper for 'enrichBy'
 maybeEnrichBy :: SchemaTree
               -> PathEqPair
               -> Maybe (SchemaTree, Subst)
 maybeEnrichBy t (eqLhs, eqVal) = do
-  node <- seekCoanchor eqLhs t
-  case eqLhs of
-    PeqFeat _ eqTop eqAtt -> do
-      let (get, set) = case eqTop of
-                         Top     -> (gup,   \n x -> n { gup = x })
-                         Bottom  -> (gdown, \n x -> n { gdown = x})
-      (fs, sub) <- hush $ enrichFeat (AvPair eqAtt eqVal) (get node)
-      let t2 = fixNode (set node fs) (replace sub t)
-      return (t2, sub)
-    PeqLex _ -> do
-       vs <- gConstraints eqVal
-       let node2 = node { glexeme = FL.fromFL vs }
-           t2    = fixNode node2 t
-       return (t2, Map.empty)
- where
-  fixNode n mt = mt { tree = repNodeByNode (matchNodeName eqLhs) n (tree mt) }
+    node <- seekCoanchor eqLhs t
+    case eqLhs of
+        PeqFeat _ eqTop eqAtt -> do
+            let (get, set) = case eqTop of
+                               Top     -> (gup,   \n x -> n { gup = x })
+                               Bottom  -> (gdown, \n x -> n { gdown = x})
+            (fs, sub) <- hush $ enrichFeat (AvPair eqAtt eqVal) (get node)
+            let t2 = fixNode (set node fs) (replace sub t)
+            return (t2, sub)
+        PeqLex _ -> do
+             vs <- gConstraints eqVal
+             let node2 = node { glexeme = FL.fromFL vs }
+                 t2    = fixNode node2 t
+             return (t2, Map.empty)
+  where
+    fixNode n mt =
+        mt { tree = repNodeByNode (matchNodeName eqLhs) n (tree mt) }
 
 -- | @enrichFeat av fs@ attempts to unify @av@ with @fs@
 --
@@ -360,15 +370,15 @@ enrichFeat :: MonadUnify m
 enrichFeat (AvPair a v) fs =
     case span (\x -> avAtt x < a) fs of
         (before,here:after) | avMatch here -> do
-            let (AvPair _ (SchemaVal fv)) = here
-            (v2,sub) <- unify fv (replicate (length fv) v)
-            let av2 = AvPair a (SchemaVal v2)
-                fs2 = replace sub before ++ (av2 : replace sub after)
-            return (fs2, sub)
+             let (AvPair _ (SchemaVal fv)) = here
+             (v2,sub) <- unify fv (replicate (length fv) v)
+             let av2 = AvPair a (SchemaVal v2)
+                 fs2 = replace sub before ++ (av2 : replace sub after)
+             return (fs2, sub)
         (before,after) -> do
-            let av2 = AvPair a (SchemaVal [v])
-                fs2 = before ++ (av2 : after)
-            return (fs2, Map.empty)
+             let av2 = AvPair a (SchemaVal [v])
+                 fs2 = before ++ (av2 : after)
+             return (fs2, Map.empty)
   where
     avMatch (AvPair fa _) = fa == a
 
@@ -376,22 +386,22 @@ enrichFeat (AvPair a v) fs =
 --   that were not found in @t@
 missingCoanchors :: LexEntry -> SchemaTree -> [Text]
 missingCoanchors lexEntry t =
-   [ name eqLhs | eqLhs <- nubBy ((==) `on` name) equations, missing eqLhs ]
- where
-   equations = map fst . snd . fst . runWriter $ lexEquations lexEntry
-   name (PeqFeat n _ _) = n
-   name (PeqLex  n)     = n
-   missing eqLhs = isNothing (seekCoanchor eqLhs t)
+    [ name eqLhs | eqLhs <- nubBy ((==) `on` name) equations, missing eqLhs ]
+  where
+    equations = map fst . snd . fst . runWriter $ lexEquations lexEntry
+    name (PeqFeat n _ _) = n
+    name (PeqLex  n)     = n
+    missing eqLhs = isNothing (seekCoanchor eqLhs t)
 
 -- | Split a lex entry's path equations into interface enrichement equations
 --   or (co-)anchor modifiers
 lexEquations :: LexEntry -> Writer [LexCombineError] ([AvPair GeniVal],[PathEqPair])
 lexEquations =
-  fmap myPartition . mapM parseAv . iequations
+    fmap myPartition . mapM parseAv . iequations
   where
-   myPartition xs = ( [ AvPair a v | (PeqInterface a, v) <- xs ]
-                    , [ (n,v)      | (PeqJust n, v)      <- xs ] )
-   parseAv (AvPair a v) = fmap (\a2 -> (a2,v)) (parsePathEq a)
+    myPartition xs = ( [ AvPair a v | (PeqInterface a, v) <- xs ]
+                     , [ (n,v)      | (PeqJust n, v)      <- xs ] )
+    parseAv (AvPair a v) = fmap (\a2 -> (a2,v)) (parsePathEq a)
 
 -- | @seekCoanchor lhs t@ returns @Just node@ if @t@ contains exactly one
 --   node that can be identified by @lhs@, @Nothing@ if it contains none.
